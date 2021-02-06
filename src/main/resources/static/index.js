502b68263c994749c75dd0d3f048bc520d3a2e5a
LeightonManningQA <lmanning@QA.com>
2021-02-06 20:22:57 +0000

    still working on javascript for frontend

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -9,42 +9,12 @@ const createtask3 = document.querySelector("#task3input");
 
 
 
-
-const retrieveData = () => {
-    fetch("http://localhost:8082/todo/create")
-    .then((response) => {
-        // check that the response is OK (i.e. 200)
-        if(response.status !== 200){
-            throw new Error("I don't have a status of 200");
-        }else{
-            console.log(response);
-            console.log(`response is OK (200)`);
-            //json-ify it (which returns a promise)
-            response.json().then((infofromserver) =>{
-                console.log(infofromserver);
-                console.log(infofromserver.data); // key - return array(6)
-                for(let users of infofromserver.data){
-                    console.log(users.first_name);
-                    printNameToScreen(users.first_name);
-                }
-            })
-        }
-    }).catch((err) => {
-        console.error(err);
-    })
-}
-
-
-
-
 const createToDo = () => {
 
     const toDoHeading = ToDoTitle.value;
 
- 
     let data = {
         title: toDoHeading,
-        taskList:[{description:task1},{description:task2},{description:task3}]
     }
     console.log(data);
 
@@ -59,10 +29,27 @@ const createToDo = () => {
         .then(response => response.json())
         .then(info => {
             console.log(info);
+            return info.retrieveData(id)
         })
         .catch(err => console.error('ERROR!' + err));
 }
-// const updateToDo = () => {
+
+const readAllTodo = () => {
+    fetch("http://localhost:8082/todo/readAll")
+    .then(response => response.json())
+    .then(info => {
+        for(let todo of info) {
+            console.log(todo);
+        }
+    } 
+        
+        
+        
+        )
+}
+
+
+// const createTask = () => {
 //     const task1 = createtask1.value
 //     const task2 = createtask2.value
 //     const task3 = createtask3.value
