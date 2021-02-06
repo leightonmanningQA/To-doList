a4a1c3b29c5d8d3e460d1526d114ec03505c942f
LeightonManningQA <lmanning@QA.com>
2021-02-06 19:12:27 +0000

    added some fields and started working on the javascript required to take data from them.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,25 +1,50 @@
 'use strict'
 
-const createToDo = document.querySelector("#createToDo");
+const ToDoTitle = document.querySelector("#ToDoTitle");
 
 const createtask1 = document.querySelector("#task1input");
 const createtask2 = document.querySelector("#task2input");
 const createtask3 = document.querySelector("#task3input");
 
+
+
+
+
+const retrieveData = () => {
+    fetch("http://localhost:8082/todo/create")
+    .then((response) => {
+        // check that the response is OK (i.e. 200)
+        if(response.status !== 200){
+            throw new Error("I don't have a status of 200");
+        }else{
+            console.log(response);
+            console.log(`response is OK (200)`);
+            //json-ify it (which returns a promise)
+            response.json().then((infofromserver) =>{
+                console.log(infofromserver);
+                console.log(infofromserver.data); // key - return array(6)
+                for(let users of infofromserver.data){
+                    console.log(users.first_name);
+                    printNameToScreen(users.first_name);
+                }
+            })
+        }
+    }).catch((err) => {
+        console.error(err);
+    })
+}
+
+
+
+
 const createToDo = () => {
 
-    const toDoHeading = createToDo.value;
-    const task1 = createtask1.value
-    const task2 = createtask2.value
-    const task3 = createtask3.value
+    const toDoHeading = ToDoTitle.value;
+
  
     let data = {
         title: toDoHeading,
-        taskList: [
-            task1,
-            task2,
-            task3
-        ]
+        taskList:[{description:task1},{description:task2},{description:task3}]
     }
     console.log(data);
 
@@ -37,3 +62,26 @@ const createToDo = () => {
         })
         .catch(err => console.error('ERROR!' + err));
 }
+// const updateToDo = () => {
+//     const task1 = createtask1.value
+//     const task2 = createtask2.value
+//     const task3 = createtask3.value
+//     let data = {
+//         taskList:[{description:task1},{description:task2},{description:task3}]
+//     }
+//     console.log(data);
+//     fetch("http://localhost:8082/task/create", {
+//         method: "POST",
+//         body: JSON.stringify(data),
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Access-Control-Allow-Origin": "*"
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(info => {
+//             console.log(info);
+//         })
+//         .catch(err => console.error('ERROR!' + err));
+        
+// }
