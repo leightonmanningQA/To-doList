abaa4d83c30c01745ad9f71b4f942a34f9cc12ee
LeightonManningQA <lmanning@QA.com>
2021-02-12 09:51:16 +0000

    Finished the unit tests for both todo and tasks

--- src/test/java/com/qa/services/ToDoServicesUnitTest.java
+++ src/test/java/com/qa/services/ToDoServicesUnitTest.java
@@ -1,5 +1,7 @@
 package com.qa.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -89,13 +91,58 @@ public class ToDoServicesUnitTest {
 		
 		
 	}
-//	@Test
-//	public void update() {
-//		
-//	}
-//	@Test
-//	public void readAll() {
-//		
-//	}
+	@Test
+	public void update() {
+		// RESOURCES
+		ToDoDomain TEST_TODO = new ToDoDomain(1L, "Chore List", null);
+		ToDoDomain UPDATEDTODO = new ToDoDomain(1L, "Shopping", null);
+		ToDoDTO EXPECTED = new ToDoDTO(1L, "Chore List");
+
+		// Rules
+		Mockito.when(this.mockrepo.findById(1L)).thenReturn(Optional.of(TEST_TODO));
+		Mockito.when(this.mockrepo.save(Mockito.any(ToDoDomain.class))).thenReturn(UPDATEDTODO);
+		Mockito.when(this.mapper.map(UPDATEDTODO, ToDoDTO.class)).thenReturn(EXPECTED);
+
+		// Actions
+		ToDoDTO RESULT = this.service.update(1L, UPDATEDTODO);
+
+		// Assertions
+		Assertions.assertThat(RESULT).isNotNull();
+		Assertions.assertThat(RESULT).isEqualTo(EXPECTED);
+		Assertions.assertThat(RESULT).usingRecursiveComparison().isEqualTo(EXPECTED);
+		Mockito.verify(this.mockrepo, Mockito.times(1)).save(Mockito.any(ToDoDomain.class));
+		Mockito.verify(this.mapper, Mockito.times(1)).map(UPDATEDTODO, ToDoDTO.class);
+
+	}
+	@Test
+	public void readAll() {
+		//RESOURCES
+		ToDoDomain TEST_TODO = new ToDoDomain(1L,"Mopping", null);
+		ToDoDomain TEST_TODO2 = new ToDoDomain(2L,"Hoovering", null);
+		ToDoDTO TEST_DTO = new ToDoDTO(1L,"Mopping");
+		ToDoDTO TEST_DTO2 = new ToDoDTO(2L,"Hoovering");
+		
+		List<ToDoDomain> TODO_LIST = new ArrayList<>();
+		List<ToDoDTO> DTO_LIST = new ArrayList<>();
+		TODO_LIST.add(TEST_TODO);
+		TODO_LIST.add(TEST_TODO2);
+		DTO_LIST.add(TEST_DTO);
+		DTO_LIST.add(TEST_DTO2);
+		
+		//RULES
+		Mockito.when(this.mockrepo.findAll()).thenReturn(TODO_LIST);
+		Mockito.when(this.mapper.map(TEST_TODO, ToDoDTO.class)).thenReturn(TEST_DTO);
+		Mockito.when(this.mapper.map(TEST_TODO2, ToDoDTO.class)).thenReturn(TEST_DTO2);
+		
+		//Actions
+		List<ToDoDTO> result = this.service.readAll();
+		
+		//ASSERTIONS
+		Assertions.assertThat(result).usingRecursiveComparison().isEqualTo(DTO_LIST);
+		Assertions.assertThat(result).isEqualTo(DTO_LIST);
+		Mockito.verify(this.mapper, Mockito.times(1)).map(TEST_TODO, ToDoDTO.class);
+		Mockito.verify(this.mapper, Mockito.times(1)).map(TEST_TODO2, ToDoDTO.class);
+		Mockito.verify(this.mockrepo, Mockito.times(1)).findAll();
+	}
 
 }
